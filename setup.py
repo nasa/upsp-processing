590728b4e946055cbf871a2db1960e243544e1bb
Marc Shaw-Lecerf <marc.a.shaw-lecerf@nasa.gov>
2023-05-24 10:25:14 -0400

    skbuild: skip_cmake=True unless sys.platform=linux
    This is a bit of a hack but it allows us to build a "pure"
    Python package without the C/C++ extensions. There are some
    Linux-specific routines that must be handled carefully before
    re-enabling the CMake build for cross-platform support.

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
+import sys
 from skbuild import setup
 from setuptools import find_packages
+
+# TODO enable cross-platform support for C/C++ extensions.
+skip_cmake = False if sys.platform in ['linux'] else True
 
 setup(
     packages=find_packages(where="python"),
     package_dir={"": "python"},
     include_package_data=True,
     package_data={"upsp.processing": ["templates/*.template"]},
+    skip_cmake=skip_cmake
 )
