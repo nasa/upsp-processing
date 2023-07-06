ed53e3246ebc15a6b79aa17f9e835952869bd293
Kenneth Lyons <kenneth.r.lyons@nasa.gov>
2023-07-06 12:31:47 -0700

    Initial migration to scikit-build-core

--- setup.py
+++
@@ -1,9 +0,0 @@
-from skbuild import setup
-from setuptools import find_packages
-
-setup(
-    packages=find_packages(where="python"),
-    package_dir={"": "python"},
-    include_package_data=True,
-    package_data={"upsp.processing": ["templates/*.template"]},
-)
